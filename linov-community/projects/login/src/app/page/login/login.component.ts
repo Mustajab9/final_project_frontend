a38a6b07f4bbf9e54bd58eb87768f00884548015
48911414+CardinalSAO@users.noreply.github.com <48911414+CardinalSAO@users.noreply.github.com>
2022-04-05 08:59:13 +0700

    Connection Login Frontend and Backend

--- linov-community/projects/login/src/app/page/login/login.component.ts
+++ linov-community/projects/login/src/app/page/login/login.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { LoginDtoReq } from '../../../../../core/src/app/dto/user/login-dto-req';
+import { LoginService } from '../../../../../core/src/app/service/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  login: LoginDtoReq = new LoginDtoReq()
+  private loginSubscription?: Subscription
+
+  constructor(private title: Title, private router: Router, private loginService: LoginService) {
+    this.title.setTitle('Login')
+  }
 
   ngOnInit(): void {
   }
 
+  onLogin(isValid: boolean) {
+    if (isValid) {
+      this.loginSubscription = this.loginService.login(this.login).subscribe(result => {
+        this.loginService.saveData(result)
+        this.router.navigateByUrl('/dashboard')
+      })
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.loginSubscription?.unsubscribe()
+  }
 }
--- linov-community/projects/login/src/app/page/login/login.module.ts
+++ linov-community/projects/login/src/app/page/login/login.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from "@angular/forms";
 import { LoginComponent } from "./login.component";
 import { LoginRouter } from "./login.router";
 import {PasswordModule} from 'primeng/password';
-import { ComponentModule } from "projects/core/src/app/component/components.module";
+import { ComponentModule } from "../../../../../core/src/app/component/components.module";
 
 @NgModule({
     declarations : [
