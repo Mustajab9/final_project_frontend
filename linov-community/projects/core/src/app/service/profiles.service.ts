459e13bdd8ca3bef59e708a7faa1790eb7bcec65
wiznuu <awisnu56@gmail.com>
2022-04-07 14:33:32 +0700

    Update Profile Service

--- linov-community/projects/core/src/app/service/profiles.service.ts
+++ linov-community/projects/core/src/app/service/profiles.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs'
 import { DeleteByProfilesIdDtoRes } from '../dto/profiles/delete-by-profiles-id-dto-res'
 import { GetAllProfilesDtoRes } from '../dto/profiles/get-all-profiles-dto-res'
 import { GetByProfilesIdDtoRes } from '../dto/profiles/get-by-profiles-id-dto-res'
+import { GetProfileByUserDtoRes } from '../dto/profiles/get-profile-by-user-dto-res'
 import { InsertProfilesDtoReq } from '../dto/profiles/insert-profiles-dto-req'
 import { InsertProfilesDtoRes } from '../dto/profiles/insert-profiles-dto-res'
 import { UpdateProfilesDtoReq } from '../dto/profiles/update-profiles-dto-req'
@@ -15,26 +16,30 @@ import { UpdateProfilesDtoRes } from '../dto/profiles/update-profiles-dto-res'
 })
 
 export class ProfilesService {
-    constructor(private http: HttpClient){
+    constructor(private http: HttpClient) {
     }
 
-    getAll() : Observable<GetAllProfilesDtoRes> {
+    getAll(): Observable<GetAllProfilesDtoRes> {
         return this.http.get<GetAllProfilesDtoRes>(`http://localhost:8080/profiles`)
     }
 
-    getById(id: string) : Observable<GetByProfilesIdDtoRes> {
+    getById(id: string): Observable<GetByProfilesIdDtoRes> {
         return this.http.get<GetByProfilesIdDtoRes>(`http://localhost:8080/profiles/${id}`)
     }
 
-    insert(insertReq: InsertProfilesDtoReq) : Observable<InsertProfilesDtoRes> {
+    getByUserId(id: string): Observable<GetProfileByUserDtoRes> {
+        return this.http.get<GetProfileByUserDtoRes>(`http://localhost:8080/profiles/user/${id}`)
+    }
+
+    insert(insertReq: InsertProfilesDtoReq): Observable<InsertProfilesDtoRes> {
         return this.http.post<InsertProfilesDtoRes>(`http://localhost:8080/profiles`, insertReq)
     }
 
-    update(updateReq: UpdateProfilesDtoReq) : Observable<UpdateProfilesDtoRes> {
+    update(updateReq: UpdateProfilesDtoReq): Observable<UpdateProfilesDtoRes> {
         return this.http.put<UpdateProfilesDtoRes>(`http://localhost:8080/profiles`, updateReq)
     }
 
-    delete(id: string) : Observable<DeleteByProfilesIdDtoRes> {
+    delete(id: string): Observable<DeleteByProfilesIdDtoRes> {
         return this.http.delete<DeleteByProfilesIdDtoRes>(`http://localhost:8080/profiles/${id}`)
     }
 }
